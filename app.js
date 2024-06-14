7a6abf1f6d0640a5b3f50117dd716b5793031828
Himanshu <kyadav.9643@gmail.com>
2024-06-14 15:40:07 +0530

    chore: Add CORS middleware for cross-origin resource sharing

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import userRouter from "./routes/userRouter.js";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
@@ -9,6 +10,7 @@ export const app = express();
 
 //using middlewares
 app.use(express.json());
+app.use(cors())
 
 //routes
 app.use("/api/users", userRouter);
