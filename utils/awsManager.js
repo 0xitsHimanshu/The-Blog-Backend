fc454a818c8afce35200d31b9e0da4526c3301a4
Himanshu <kyadav.9643@gmail.com>
2024-06-28 22:02:31 +0530

    refactor: Update AWS configuration in awsManager.js

--- utils/awsManager.js
+++ utils/awsManager.js
@@ -1,28 +1,10 @@
 import aws from "aws-sdk";
 import { nanoid } from "nanoid";
 
-// export const s3 = new aws.S3({
-//     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-//     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-//     region: process.env.AWS_REGION,
-// });
-
-// export const generateURL = async () =>{
-//     const date = new Date();
-//     const  imageName = `${nanoid()}-${date.getTime()}.jpeg`;
-
-//     return await s3.getSignedUrlPromise({
-//         Bucket: "theblogbucket",
-//         Key: imageName,
-//         ContentType: "image/jpeg",
-//         Expires: 1000,
-//     })
-// }
-
 export const s3 = new aws.S3({
+    region: process.env.AWS_REGION,
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_REGION,
 });
 
 export const generateURL = async () => {
@@ -33,11 +15,13 @@ export const generateURL = async () => {
         Bucket: "theblogbucket",
         Key: imageName,
         ContentType: "image/jpeg",
-        Expires: 1000, // Ensure this is a number, not a string
+        Expires: 1000 // Ensure this is a number, not a string
     };
 
     // Add debugging statements
-    console.log('Params:', params);
+    //uncomment for just checking the params
+    
+    // console.log('Params:', params);
 
     try {
         const url = await s3.getSignedUrlPromise('putObject', params);
