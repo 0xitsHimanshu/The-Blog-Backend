a097d41699274c2424497389e0d291fcece7f892
Himanshu <kyadav.9643@gmail.com>
2024-07-23 00:07:00 +0530

    feat: Add validation for blog creation inputs

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -9,14 +9,17 @@ export const createBlog = async (req, res) => {
 
    if(!title.length)
         return res.status(403).json({"error": "You must provide a title"})
-    if(!des.length || des.length > 200)
-        return res.status(403).json({"error": "You must provide a description of the blog within 200 characters"})
-    if(!banner.length)
-        return res.status(403).json({"error": "You must provide a blog banner to publish it"});
-    if(!content.blocks.length)
-        return res.status(403).json({"error": "There must be some blog content to publish it"});
-    if(!tags.length || tags.length > 10)
-        return res.status(403).json({"error": "Provide tags in order to publish the blog, Maximum 10"});
+
+   if(!draft){
+       if(!des.length || des.length > 200)
+           return res.status(403).json({"error": "You must provide a description of the blog within 200 characters"})
+       if(!banner.length)
+           return res.status(403).json({"error": "You must provide a blog banner to publish it"});
+       if(!content.blocks.length)
+           return res.status(403).json({"error": "There must be some blog content to publish it"});
+       if(!tags.length || tags.length > 10)
+           return res.status(403).json({"error": "Provide tags in order to publish the blog, Maximum 10"});
+   }
 
     tags = tags.map(tag => tag.toLowerCase());
 
