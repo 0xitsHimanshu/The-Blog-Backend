30e4af1d770a6f6df184fbb790a7dab122b6fb96
Himanshu <kyadav.9643@gmail.com>
2024-07-23 23:34:30 +0530

    feat: Add searchBlogs functionality to blogController and blogRouter

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -77,3 +77,21 @@ export const getTrendingBlog = (req,res) => {
         return res.status(500).json({"error": err.message})
      })
 };
+
+export const searchBlogs = (req, res) => {
+    let {tag} = req.body;
+    let findQuery = {"tags": tag, "draft": false};
+    let maxLimit = 5;
+
+    Blog.find(findQuery)
+    .populate("author", "personal_info.profile_img personal_info.username personal_info.fullname -_id")
+    .sort({"publishedAt": -1})
+    .select("blog_id title des banner activity tags publishedAt -_id")
+    .limit(maxLimit)
+    .then( blogs => {
+       return res.status(200).json({blogs})
+    })
+    .catch(err => {
+       return res.status(500).json({"error": err.message})
+    })
+};
--- routes/blogRouter.js
+++ routes/blogRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { createBlog, getLatestBlog, getTrendingBlog } from "../controllers/blogController.js";
+import { createBlog, getLatestBlog, getTrendingBlog, searchBlogs } from "../controllers/blogController.js";
 import { verifyJWT } from "../utils/helper.js";
 
 const route = express.Router();
 
 route.get("/latest-blogs",getLatestBlog)
 route.get("/trending-blogs", getTrendingBlog)
+route.post("/search-blogs", searchBlogs);
 route.post("/create-blog", verifyJWT ,createBlog);
 
 export default route;
