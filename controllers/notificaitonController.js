581463bc9253920a7017052cecf47a7ce9033980
Himanshu <kyadav.9643@gmail.com>
2024-08-17 16:00:23 +0530

    feat: Add get_notifications and all_notifications_count functions

--- controllers/notificaitonController.js
+++ controllers/notificaitonController.js
@@ -17,3 +17,56 @@ export const newNotification = (req, res) => {
         res.status(500).json({message: err.message})
     })
 }
+
+export const get_notifications = (req, res) => {
+    let user_id = req.user.id;
+    let {page, filter, deletedDocCount} = req.body;
+
+    let maxLimit = 10;
+    
+    let findQuery = {notification_for: user_id, user: {$ne: user_id}}
+    let skipDocs = (page-1) * maxLimit;
+    
+    if(filter != 'all'){
+        findQuery.type = filter;
+    }
+    if(deletedDocCount){
+        skipDocs -= deletedDocCount;
+    }
+
+    Notification.find(findQuery)
+    .skip(skipDocs)
+    .limit(maxLimit)
+    .populate("blog", "title blog_id")
+    .populate("user", "personal_info.fullname personal_info.username personal_info.profile_img")
+    .populate("comment", "comment")
+    .populate("replied_on_comment", "comment")
+    .populate("reply", "comment")
+    .sort({createdAt: -1})
+    .select("createdAt type seen reply")
+    .then((notifications) => {
+        res.status(200).json({notifications})
+    })
+    .catch(err => {
+        console.log(err, "error")
+        res.status(500).json({message: err.message})
+    })
+}
+
+export const all_notifications_count = (req, res) => {
+    let user_id = req.user.id;
+    let {filter} = req.body;
+    let findQuery = {notification_for: user_id, user: {$ne: user_id}}
+
+    if(filter != 'all'){
+        findQuery.type = filter;
+    }
+
+    Notification.countDocuments(findQuery)
+    .then(count => {
+        return res.status(200).json({count})
+    })
+    .catch(err => {
+        return res.status(500).json({message: err.message})
+    })
+}
--- routes/notificationRouter.js
+++ routes/notificationRouter.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { newNotification } from '../controllers/notificaitonController.js';
+import { all_notifications_count, get_notifications, newNotification } from '../controllers/notificaitonController.js';
 import { verifyJWT } from '../utils/helper.js';
 
 const route = express.Router();
 
 route.get("/new-notification",verifyJWT ,newNotification);
+route.post("/notifications", verifyJWT, get_notifications);
+route.post("/all-notifications-count", verifyJWT, all_notifications_count);
 
 export default route;
